a9ec4b26f50c65ad47ec45de1355cd6d1fba223b
Pramveer Kumar <pramveerkumarai99@gmail.com>
2025-07-03 08:08:01 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+enemyX += enemyX_change
+
+    # if enemyX <=0:
+    #     enemyX_change= 0.1
+    #     enemyY += enemyY_change
+    # elif enemyX >=736:
+    #    enemyX_change = -0.1
+    #    enemyY += enemyY_change
